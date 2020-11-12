45cf7e0f3ee0811537c5e0213856af0908abfd7b
Adam Derbidge <adam9417@gmail.com>
2020-11-12 11:20:20 -0500

    Install Generate Target, Compare Guesses, and Update Score functions

--- number-guesser-starting/script.js
+++ number-guesser-starting/script.js
@@ -3,4 +3,24 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
+function generateTarget(){
+  return Math.floor(Math.random() * 10);
+};
 
+function compareGuesses(humanGuess, computerGuess, secretTarget) {
+  if (Math.abs(humanGuess - secretTarget) < Math.abs(compareGuess - secretTarget)) {
+    return true;
+  } else if (Math.abs(humanGuess - secretTarget) === Math.abs(compareGuess - secretTarget)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+function updateScore(winnerString) {
+  if (winnerString === 'human') {
+    humanScore++;
+  } else {
+    computerScore++
+  }
+};
