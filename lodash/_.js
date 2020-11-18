ea860d46a0dcf6948f01ce9f8c3d932da73e742c
Adam Derbidge <adam9417@gmail.com>
2020-11-18 16:11:30 -0500

    Clamp passes tests

--- lodash/_.js
+++ lodash/_.js
@@ -1,7 +1,13 @@
-const _ = {}; 
+const _ = {
+  clamp(number, lower, upper) {
+    const lowerClampedValue = Math.max(number, lower);
+    const clampedValue = Math.min(lowerClampedValue, upper);
+    return clampedValue;
+  },
 
 
-
+//end of const _
+};
 
 // Do not write or modify code below this line.
 module.exports = _;
